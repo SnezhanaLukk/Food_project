fb095a9f16402284e6b23768e9949fce1d0627dc
SnezhanaLukk <157698876+SnezhanaLukk@users.noreply.github.com>
2024-11-12 13:02:32 +0300

    Action timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,54 @@ window.addEventListener('DOMContentLoaded',()=>{
 
     });
 
+    //Timer
+    const deadline = new Date('2024-11-17T00:00:00');
+    let days, hours, minutes, seconds;
+
+    function getTimeRemaining(endtime){
+        const total = Date.parse(endtime)-Date.parse(new Date());
+        if (total<=0){
+            days=0;
+            hours=0;
+            minutes=0;
+            seconds=0;
+        } else {
+            days = Math.floor(total/1000/60/60/24);
+            hours = Math.floor((total/1000/60/60)%24);
+            minutes = Math.floor((total/1000/60)%60);
+            seconds = Math.floor((total/1000)%60);
+        }
+        return {total, days, hours, minutes, seconds};
+    
+    }
+    
+    function getZero(num){
+        if(num>=0&&num<10){
+            return `0${num}`;
+        }else return num;
+    }
+    
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days=timer.querySelector('#days'),
+              hours=timer.querySelector('#hours'),
+              minutes=timer.querySelector('#minutes'),
+              seconds=timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+                days.innerHTML = getZero(t.days);
+            if (t.total<=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline);
+
 });
