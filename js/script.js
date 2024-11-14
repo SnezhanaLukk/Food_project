3f97473ebef1d832f6ce0aaf64d01e347b42cccb
SnezhanaLukk <157698876+SnezhanaLukk@users.noreply.github.com>
2024-11-14 13:25:07 +0300

    rewrote the menu card using JS and reduction and universalization

--- js/script.js
+++ js/script.js
@@ -121,7 +121,7 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
     });
 
-    const openTimer = setTimeout(openModal,5000);
+    // const openTimer = setTimeout(openModal,5000);
 
     function showModalByScroll(){
         if (window.pageYOffset + document.documentElement.clientHeight>=document.documentElement.scrollHeight - 1){
@@ -137,12 +137,13 @@ window.addEventListener('DOMContentLoaded', () => {
 // Напишем меню через классы
 
 class MenuCard{
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 98;
         this.calcPrice();
@@ -152,8 +153,14 @@ class MenuCard{
     }
     render(){
         const element = document.createElement('div');
+        if (this.classes.length===0){
+            this.element = 'menu__item';
+            element.classList.add(this.element)
+        }else{
+            this.classes.forEach(className => element.classList.add(className));
+        }
+        
         element.innerHTML=`
-        <div class="menu__item">
             <img src="${this.src}" alt="${this.alt}">
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -162,7 +169,6 @@ class MenuCard{
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
             </div>
-        </div>
         `;
         this.parent.append(element);
     }
@@ -174,8 +180,7 @@ new MenuCard (
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container'
-    
+    '.menu .container'    
 ).render();
 
 new MenuCard(
@@ -184,14 +189,16 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    '.menu .container'
+    '.menu .container',
+    'menu__item',
 ).render();
 
 new MenuCard(
     "img/tabs/elite.jpg",
     "elite",
     'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    '.menu .container'
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, мясо, фермерские овощи и фрукты - ресторанное меню без похода в ресторан!',
+    20,
+    '.menu .container',
+    'menu__item',
 ).render();
