94beb0d55a8137f44cb96f3dc745fe87ee4bf8ec
SnezhanaLukk <157698876+SnezhanaLukk@users.noreply.github.com>
2024-12-19 13:44:25 +0300

    added sliders

--- js/script.js
+++ js/script.js
@@ -209,34 +209,6 @@ window.addEventListener('DOMContentLoaded', () => {
             document.querySelector('.menu .container').append(element);
         });
     }
-    // new MenuCard(
-    //     "img/tabs/vegy.jpg",
-    //     "vegy",
-    //     'Меню "Фитнес"',
-    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    //     9,
-    //     '.menu .container'
-    // ).render();
-
-    // new MenuCard(
-    //     "img/tabs/post.jpg",
-    //     "post",
-    //     'Меню "Постное"',
-    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    //     14,
-    //     '.menu .container',
-    //     'menu__item',
-    // ).render();
-
-    // new MenuCard(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню “Премиум”',
-    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, мясо, фермерские овощи и фрукты - ресторанное меню без похода в ресторан!',
-    //     20,
-    //     '.menu .container',
-    //     'menu__item',
-    // ).render();
 
     //Forms
 
@@ -328,4 +300,47 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(data => data.json())
         .then(res => console.log(res));
 
+    // Sliders
+
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else total.textContent = slides.length;
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slideIndex < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else current.textContent = slideIndex;
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1)
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1)
+    });
 });
